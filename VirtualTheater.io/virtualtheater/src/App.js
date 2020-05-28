81385422008c3cc1d31038ffa80bfaaa84a69618
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-28 17:54:16 -0400

    added attribution

--- VirtualTheater.io/virtualtheater/src/App.js
+++ VirtualTheater.io/virtualtheater/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div>
       
+      <div>
+        <h4>Information provided by</h4>
+        <img alt="themoviedb" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg"/>
+      </div>
     </div>
   );
 }
