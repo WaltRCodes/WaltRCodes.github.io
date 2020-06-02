10fa965ae3731540061b7beed34bd1840496db89
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-06-02 10:00:44 -0400

    styling

--- VirtualTheater.io/virtualtheater/src/components/Movie.js
+++ VirtualTheater.io/virtualtheater/src/components/Movie.js
@@ -30,8 +30,9 @@ export default class Movie extends Component {
 
     
     let movieHTML = <div >
-        <div>
-            <img src={"https://image.tmdb.org/t/p/w200"+movie.poster_path} />
+        <div className="row">
+            <img src={"https://image.tmdb.org/t/p/w300"+movie.poster_path} />
+            <div className="main">
             <h4>{movie.title}</h4>
             <h5>{movie.release_date}</h5>
             <div>Runtime: {movie.runtime} mins</div>
@@ -40,6 +41,7 @@ export default class Movie extends Component {
             <p>{movie.overview}</p>
             <div>Production provided by: {movie.production_companies.map(company => <p>{company.name}</p>)}</div>
             <div>Produced in the following countries: {movie.production_countries.map(country => <p>{country.name}</p>)}</div>
+            </div>
         </div>
         <button onClick={() => {
             if(this.props.balance-19.99>0){
