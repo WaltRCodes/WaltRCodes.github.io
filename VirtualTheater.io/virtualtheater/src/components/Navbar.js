2f84319e889964f7f37dd313f2e373e5e794d59c
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-06-02 01:17:40 -0400

    added wireframe

--- VirtualTheater.io/virtualtheater/src/components/Navbar.js
+++ VirtualTheater.io/virtualtheater/src/components/Navbar.js
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="nav-bar">
-        <Link to="/">Search</Link>
-        <Link to="/Profile">Profile</Link>
+        <Link to="/">Search</Link> <br></br>
+        <Link to="/Profile">Profile</Link> <br></br>
         <Link to="/Movie">Your last viewed Movie</Link>
     </div>
   );
