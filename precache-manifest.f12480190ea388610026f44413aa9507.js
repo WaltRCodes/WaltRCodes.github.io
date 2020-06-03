a6e08e4d833f0bea72db38d91cf32496079f23c0
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-06-03 12:11:59 -0400

    Updates

---
+++ precache-manifest.f12480190ea388610026f44413aa9507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c637a175e32de3bf7c6432be5452797",
+    "url": "/virtualtheater/index.html"
+  },
+  {
+    "revision": "f61840ce6f5edb9f343d",
+    "url": "/virtualtheater/static/css/main.346f1bda.chunk.css"
+  },
+  {
+    "revision": "c1a3622e41919326a352",
+    "url": "/virtualtheater/static/js/2.45dd4929.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/virtualtheater/static/js/2.45dd4929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61840ce6f5edb9f343d",
+    "url": "/virtualtheater/static/js/main.79981d84.chunk.js"
+  },
+  {
+    "revision": "c4515765fe179f1219d5",
+    "url": "/virtualtheater/static/js/runtime-main.60bbdc99.js"
+  }
+]);
