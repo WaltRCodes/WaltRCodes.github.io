a396aaf793033d4b1da65e9f745861979738eeea
Walter <4wrada@gmail.com>
2020-06-26 23:42:31 -0400

    updated grid placement

--- VirtualTheater.io/virtualtheater/src/App.js
+++ VirtualTheater.io/virtualtheater/src/App.js
@@ -36,9 +36,9 @@ render(){
       {this.state.signedIn ?
             <BrowserRouter>
                 <Navbar />
-                <Route exact strict path="/" render={() => <div><Search capture={this.chooseMovie} userName={this.state.user.name}/></div>} />
-                <Route path="/Profile" render={() => <div><Profile capture={this.chooseMovie} userId={this.state.user.id} userName={this.state.user.name} userEmail={this.state.user.email} userAddress={this.state.user.address} userPassword={this.state.user.password} userBalance={this.state.user.balance}/></div>} />
-                <Route path="/Movie" render={() => <div><Movie id={this.state.movieId} userId={this.state.user.id} balance={this.state.user.balance} /></div>} />
+                <Route exact strict path="/" render={() => <Search capture={this.chooseMovie} userName={this.state.user.name}/>} />
+                <Route path="/Profile" render={() => <Profile capture={this.chooseMovie} userId={this.state.user.id} userName={this.state.user.name} userEmail={this.state.user.email} userAddress={this.state.user.address} userPassword={this.state.user.password} userBalance={this.state.user.balance}/>} />
+                <Route path="/Movie" render={() => <Movie id={this.state.movieId} userId={this.state.user.id} balance={this.state.user.balance} />} />
                 <div className="main">
                   <h4>Information provided by TheMoviedb</h4>
                 </div>
@@ -46,8 +46,8 @@ render(){
             
         :
             <BrowserRouter>
-                <Route exact strict path="/" render={() => <div><SignIn Allowed={this.signingIn}/></div>} />
-                <Route path="/SignUp" render={() => <div><SignUp/></div>} />
+                <Route exact strict path="/" render={() => <SignIn Allowed={this.signingIn}/>} />
+                <Route path="/SignUp" render={() => <SignUp/>} />
             </BrowserRouter>
         }
     </div>
