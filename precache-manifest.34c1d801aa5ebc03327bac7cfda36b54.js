74fbc492428bceaae21afa33ba2fe13b2289fb72
Walter <4wrada@gmail.com>
2020-07-21 16:37:54 -0400

    Updates

---
+++ precache-manifest.34c1d801aa5ebc03327bac7cfda36b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea18f21445e952017400ea08efeca90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e411800e3289256be49",
+    "url": "/static/css/main.8c0e8b3d.chunk.css"
+  },
+  {
+    "revision": "2662e51647cf66d85b16",
+    "url": "/static/js/2.37cbb32f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.37cbb32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e411800e3289256be49",
+    "url": "/static/js/main.06237906.chunk.js"
+  },
+  {
+    "revision": "6dc2f87c57d98cf48c5d",
+    "url": "/static/js/runtime-main.5dde516b.js"
+  }
+]);
